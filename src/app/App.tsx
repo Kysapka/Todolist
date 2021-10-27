be677a4a72f5ed3206c19527ad5146e63550236b
Артем <kysapka@gmail.com>
2021-10-27 19:08:43 +0300

    add appReducer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,15 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Container} from "@material-ui/core";
+import {Container, LinearProgress} from "@material-ui/core";
 import {TodolistsList} from "../features/TodolistsList/TodolistsList";
+import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
 
 export const App = React.memo(() => {
     return (
         <div>
-            <Container fixed>
+            <Container maxWidth="xl">
+                <ErrorSnackbar />
                 <AppBar position="static" style={{marginBottom: 30}}>
                     <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
                         <div style={{display: 'flex', alignItems: 'center'}}>
@@ -25,7 +27,9 @@ export const App = React.memo(() => {
                         </div>
                         <Button color="inherit">Login</Button>
                     </Toolbar>
+                    <LinearProgress />
                 </AppBar>
+
                 <TodolistsList />
             </Container>
         </div>
---
+++ src/app/AppReducer.ts
@@ -0,0 +1,21 @@
+type InitialStateType = {
+    status: 'idle' | 'loading' | 'succeeded' | 'failed',
+    error: string | null
+}
+
+const initialState: InitialStateType = {
+    status: 'idle',
+    error: null
+}
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET_STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET_ERROR':
+            return {...state, error: action.error}
+        default: return {...state}
+    }
+}
+
+type  ActionsType = any
