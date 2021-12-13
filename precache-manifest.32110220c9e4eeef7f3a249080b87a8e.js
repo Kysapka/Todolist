1d983493a1903a0e935c25c921fec90e5d5eac62
Артем <kysapka@gmail.com>
2021-12-13 23:41:03 +0300

    Updates

---
+++ precache-manifest.32110220c9e4eeef7f3a249080b87a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f686aeef3fc0a16d2be36c70c1031b7",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "c69d81accaa8d9ee0473",
+    "url": "/Todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "303ccf4a1ec5bba5a275",
+    "url": "/Todolist/static/js/2.79cbf364.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.79cbf364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69d81accaa8d9ee0473",
+    "url": "/Todolist/static/js/main.2172ca3a.chunk.js"
+  },
+  {
+    "revision": "4802ac18793f8b07d15e",
+    "url": "/Todolist/static/js/runtime-main.5cd23dcc.js"
+  }
+]);
