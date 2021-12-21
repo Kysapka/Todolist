dab700bcc08d3d79b40c86bde1f94cac30b49415
Артем <kysapka@gmail.com>
2021-12-21 21:53:10 +0300

    remove hard constants

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,12 +4,10 @@ import {GetTasksResponse, LoginParamsType, ResponseType, TaskType, TodolistType,
 const settings = {
     withCredentials: true,
     headers: {
-        // 'API-KEY': '9660a6e9-744c-4376-8717-32b82016bc28'
         'API-KEY': process.env.REACT_APP_API_KEY
     }
 }
 const instance = axios.create({
-    // baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     baseURL: process.env.REACT_APP_BASE_URL,
     ...settings
 })
